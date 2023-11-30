2d4532f5c8c29029e3226304f4999ca1fffefa2f
Brian Franco <104641779+bafranco10@users.noreply.github.com>
2023-11-30 18:42:30 -0500

    Add files via upload

---
+++ babyOS.cpp
@@ -0,0 +1,25 @@
+// FILE: makefile
+// A Bautista, B Franco, E Mora
+// OS, Fall 2023, Transy U
+//
+// driver for Group 3 babyOS 
+//
+
+#include "FCFS.h"
+#include "pageReplacementSimulator.h"
+#include "pageTable.h"
+#include "Priority.h"
+#include "procManagement.h" 
+#include "RoundRobin.h"
+#include "SJF.h"
+#include "symConsts.h"
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char **argv){
+  return 0;
+}
---
+++ makefile
@@ -0,0 +1,11 @@
+# FILE: makefile
+# A Bautista, B Franco, E Mora
+# OS, Fall 2023, Transy U
+#
+# makefile for Group 3 Final
+
+babyOS: babyOS.o FCFS.o pageReplacementSimulator.o pageTable.o Priority.o RoundRobin.o SJF.o
+	g++ babyOS.o FCFS.o pageReplacementSimulator.o pageTable.o Priority.o RoundRobin.o SJF.o 
+
+babyOS.o: FCFS.h pageReplacementSimulator.h pageTable.h Priority.h procManagement.h RoundRobin.h SJF.h symConsts.h
+	g++ -c babyOS.cpp
